0137895e800dcca076ded8bec2cded7cfc5cfdc4
Tim Schmelmer <tim.schmelmer@gmail.com>
2017-11-17 22:23:31 -0700

    changing the text view text in the MinaActivity

--- app/src/main/java/com/timschmelmer/candycoded/MainActivity.kt
+++ app/src/main/java/com/timschmelmer/candycoded/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.timschmelmer.candycoded
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.text_view_title)
+
+        textView.text = getString(R.string.products_title)
+
     }
 }
